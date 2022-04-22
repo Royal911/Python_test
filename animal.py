52d8b7c42b1bad2fb43a6e344fdce16c1c068f30
Royal911 <dan.buda@yahoo.com>
2022-04-22 19:42:03 +0000

    Nope

--- animal.py
+++ animal.py
@@ -1,2 +1,7 @@
-print("What animal is this? ..Oink!")
-print("YOU GUESS!!...Is the pig!")
+my_list = [10, 1, 8, 3, 5]
+total = 0
+
+for i in range(len(my_list)):
+    total += my_list[i]
+
+print(total)
